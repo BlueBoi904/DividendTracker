e080d91282c287d5ceea0268db19fcaa9c35551e
BlueBoi904 <cliff.mirschel@gmail.com>
2021-06-04 19:01:37 -0700

    Fixed edit mode, added auto focus when pressing enter, fixed edit call and button logic

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -28,9 +28,9 @@ function TodoList({ getTodos, todos }: TodoListProps) {
       console.log(error);
     }
   };
-  const editTodo = async (todo: TodoType) => {
+  const editTodo = async (todo: TodoType, task: string) => {
     try {
-      todo.completed = true;
+      todo.task = task;
       const res = await axios.put(todoUrl + todo.id, todo);
       getTodos();
     } catch (error) {
@@ -57,6 +57,7 @@ function TodoList({ getTodos, todos }: TodoListProps) {
           completeTodo={completeTodo}
           setCurrentTodo={setCurrentTodo}
           currentTodo={currentTodo}
+          editTodo={editTodo}
         />
       );
     });
@@ -65,12 +66,13 @@ function TodoList({ getTodos, todos }: TodoListProps) {
 }
 
 type TodoItemTypes = {
-    item: any,
-    completeTodo: any,
-    deleteTodo: any,
-    setCurrentTodo: any,
-    currentTodo: any
-}
+  item: any;
+  completeTodo: any;
+  deleteTodo: any;
+  setCurrentTodo: any;
+  currentTodo: any;
+  editTodo: any;
+};
 
 function TodoItem({
   item,
@@ -78,26 +80,26 @@ function TodoItem({
   deleteTodo,
   setCurrentTodo,
   currentTodo,
+  editTodo,
 }: TodoItemTypes) {
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCurrentTodo(e.target.value);
+  };
+
+  const handleInitialEditValue = (value: any) => {
+    setCurrentTodo(value);
   };
   const [editMode, setEditMode] = useState<boolean>(false);
   const inputRef = useRef<null | HTMLInputElement>(null);
 
   useEffect(() => {
-      if (editMode) {
-        inputRef.current?.focus?.()
-      }
-  }, [editMode])
+    if (editMode) {
+      inputRef.current?.focus?.();
+    }
+  }, [editMode]);
 
   return (
     <div
-      onClick={() => {
-        if (!editMode) {
-          completeTodo(item);
-        }
-      }}
       key={item.id}
       className='grid grid-cols-11 border-2 bg-gray-100  rounded-md border-blue-500 my-2'
     >
@@ -105,13 +107,23 @@ function TodoItem({
         <input
           type='text'
           ref={inputRef}
-          placeholder='Edit todo...'
           className='text-blue-400 placeholder-opacity-75 placeholder-blue-400 bg-gray-100 col-span-10 text-lg py-1 pl-2 font-normal focus:border-transparent focus:outline-none border-none rounded-md border-blue-500'
           value={currentTodo}
           onChange={handleInputChange}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              editTodo(item, currentTodo);
+              setEditMode(false);
+            }
+          }}
         />
       ) : (
         <p
+          onClick={() => {
+            if (!editMode) {
+              completeTodo(item);
+            }
+          }}
           className={`${
             item.completed && 'line-through'
           } cursor-pointer text-blue-400 col-span-10 text-lg py-1 pl-2 font-normal`}
@@ -121,27 +133,34 @@ function TodoItem({
       )}
 
       <div className='flex justify-between items-center p-1'>
-        {editMode ? (
+        <CheckCircleIcon
+          onClick={() => {
+            if (!editMode){
+                completeTodo(item);
+            }
+          }}
+          className={`h-5 w-5 ${
+            item.completed ? 'text-green-500' : 'text-blue-500'
+          } ${!editMode && ' cursor-pointer'}`}
+        />
+
+        {!editMode ? (
+          <PencilAltIcon
+            onClick={() => {
+              setEditMode(true);
+              handleInitialEditValue(item.task);
+            }}
+            className={`${!editMode && 'cursor-pointer'} h-5 w-5 text-blue-500`}
+          />
+        ) : (
           <XCircleIcon
             onClick={() => {
               setEditMode(false);
             }}
             className='h-5 w-5 cursor-pointer text-red-500'
           />
-        ) : (
-          <CheckCircleIcon
-            className={`h-5 w-5 cursor-pointer ${
-              item.completed ? 'text-green-500' : 'text-blue-500'
-            }`}
-          />
         )}
-        <PencilAltIcon
-          onClick={() => {
-            setEditMode(!editMode);
-            // editTodo(item)
-          }}
-          className={'cursor-pointer h-5 w-5 text-blue-500'}
-        />
+
         <TrashIcon
           onClick={() => {
             deleteTodo(item);
