c95b82f0ce34f7f84411c8dca8d204521dd0428b
BlueBoi904 <cliff.mirschel@gmail.com>
2021-06-07 17:36:39 -0700

    Adding tests

--- client/src/test/test.ts
+++ client/src/test/test.ts
@@ -5,3 +5,11 @@ function sum(a, b) {
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+function add_one(num) {
+  return num + 1;
+}
+
+test('adds 1 to a number', () => {
+  expect(add_one(6)).toBe(7);
+});
